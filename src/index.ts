5f2641094f47d2d1df72e021f680c8615553b6de
Tom Binford <tombinford@g.ucla.edu>
2026-02-14 11:03:43 -0800

    Proof of concept for creating a PR

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,36 @@ export default (app: Probot) => {
     });
     await context.octokit.issues.createComment(issueComment);
   });
+
+  // TODO run this code when the user has chosen a winning branch
+  /*
+  // Properties of the installation
+  const installationId = 110161046;
+  const repoOwner = "TomBinford";
+  const repoName = "treehacks-testing-repo";
+  const mainBranch = "main";
+
+  // Properties of the arena / its winner
+  const arenaPurpose = "TODO short purpose";
+  // null if the arena was created by a prompt, otherwise the issue number it is responding to
+  const arenaIssue: number | null = null;
+  const winningBranch = "test-winning-branch";
+
+  app.auth(installationId).then((github) => {
+    github.pulls.create({
+      owner: repoOwner,
+      repo: repoName,
+      title: arenaPurpose,
+      head: winningBranch,
+      base: mainBranch,
+      // TODO link to the arena in our frontend
+      body: (arenaIssue
+        ? `This PR created by TreeHacks Vibe Bot. Addresses #${arenaIssue}.`
+        : "This PR created by TreeHacks Vibe Bot."),
+    });
+  });
+  */
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
